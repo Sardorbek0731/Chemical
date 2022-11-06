dcd036c3d5c7d840b082c9d8b0b06eb1c5035bef
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-11-06 18:38:59 +0500

    optimizate code

--- main.js
+++ main.js
@@ -1,44 +1,33 @@
 window.addEventListener("DOMContentLoaded", function () {
   let burgerMenu = document.querySelector(".burger_menu"),
-  burMenu = document.querySelector('.burMenu'),
-  close = document.querySelector('.close'),
-  animClas = document.querySelector('.why_us_content1'),
-  animClas1 = document.querySelector('.why_us_content2')
-
+    burMenu = document.querySelector(".burMenu"),
+    close = document.querySelector(".close"),
+    animClas = document.querySelector(".why_us_content1"),
+    animClas1 = document.querySelector(".why_us_content2");
 
   burgerMenu.addEventListener("click", function (e) {
     e.preventDefault();
-    burMenu.classList.add('animActive')
-    burMenu.classList.remove('closeMenu')
-    burMenu.classList.remove('none')
-    setTimeout(() =>{
-      document.querySelector('.ul').classList.remove('none')
-    },300)
+    burMenu.classList.add("animActive");
+    burMenu.classList.remove("closeMenu");
+    burMenu.classList.remove("none");
+    setTimeout(() => {
+      document.querySelector(".ul").classList.remove("none");
+    }, 300);
   });
 
   close.addEventListener("click", function (e) {
     e.preventDefault();
-    burMenu.classList.remove('animActive')
-    burMenu.classList.add('closeMenu')
-    document.querySelector('.ul').classList.add('none')
-    setTimeout(() => {
-    },400);
+    burMenu.classList.remove("animActive");
+    burMenu.classList.add("closeMenu");
+    document.querySelector(".ul").classList.add("none");
+    setTimeout(() => {}, 400);
   });
- 
-  window.addEventListener('scroll',() =>{
+
+  window.addEventListener("scroll", () => {
     let scrollTop = window.scrollY;
-    if(scrollTop >= 180){
-      animClas.classList.add('opacityAnimation'
-      )
-      animClas1.classList.add('opacityAnimation'
-      )
+    if (scrollTop >= 180) {
+      animClas.classList.add("opacityAnimation");
+      animClas1.classList.add("opacityAnimation");
     }
-  })
-
-
-
-
-
-
-
+  });
 });
