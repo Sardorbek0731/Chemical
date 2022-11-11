9b3bca41507102fe034596c6814f08408b9197cd
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-11-11 18:31:06 +0500

    changed form contact

--- JS/main.js
+++ JS/main.js
@@ -24,3 +24,51 @@ closeSuccessfully.addEventListener("click", (e) => {
   orderNow.classList.add("none");
   successfully.classList.add("none");
 });
+
+// regEx
+
+let regEX = /[a-zA-Z0-9]{2,}@/;
+let regExNumber = /^[0-9]{2,}$/;
+let regExName = /^[a-zA-Z]{2,24}$/;
+
+const email = document.querySelectorAll("#email");
+const number = document.querySelectorAll("#number");
+const ism = document.querySelectorAll("#name");
+
+function regExFunc(id, type) {
+  if (type.test(id.value)) {
+    id.style.border = "1px solid green";
+  } else {
+    id.style.border = "1px solid red";
+  }
+}
+
+email.forEach((item) => {
+  item.addEventListener("keyup", () => {
+    regExFunc(item, regEX);
+  });
+});
+
+ism.forEach((item) => {
+  item.addEventListener("keyup", () => {
+    regExFunc(item, regExName);
+  });
+});
+
+number.forEach((item) => {
+  item.addEventListener("keyup", () => {
+    regExFunc(item, regExNumber);
+  });
+});
+
+// Slider
+
+const bannerAnimate = document.getElementById("banner-animate");
+
+function rightSlider() {
+  bannerAnimate.style.marginLeft = "-34.875rem";
+}
+
+function leftSlider() {
+  bannerAnimate.style.marginLeft = "0";
+}
